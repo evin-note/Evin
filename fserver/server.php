ce671d0229d49245681d0657bffd87651fcd9d1f
mrm <kei.id0305@gmail.com>
2019-05-09 10:16:28 +0900

    add ext checker

--- fserver/server.php
+++ fserver/server.php
@@ -1,4 +1,21 @@
 <?php
+
+function ext_check(string $sfile, string $mfile) {
+    $sext = pathinfo($sfile, PATHINFO_EXTENSION);
+    $sext = strtolower($sext);
+
+    if($sext != "wav") {
+        exit("[!]Sound file must be .wav");
+    }
+
+    $mext = pathinfo($mfile, PATHINFO_EXTENSION);
+    $mext = strtolower($mext);
+
+    if($mext != "jpg" && $mext != "png") {
+        exit("[!]Movie file must be .jpg or .png");
+    }
+}
+
 $dir = '/var/www/html/Evin/upload/';
 $spath = $dir . basename($_FILES['sound']['name']);
 $mpath = $dir . basename($_FILES['movie']['name']);
@@ -7,6 +24,8 @@ $tsname = $_FILES['sound']['tmp_name'];
 $tmname = $_FILES['movie']['tmp_name'];
 echo $tsname . "\n" . $spath . "<br>";
 echo $tmname . "\n" . $mpath . "<br>";
+
+ext_check($_FILES['sound']['name'], $_FILES['movie']['name']);
 
 //sound
 if(is_uploaded_file($tsname)) {
